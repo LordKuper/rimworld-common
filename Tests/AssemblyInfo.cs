29b91224bfb81917adeaf9df9397c928f2702f42
Mikhail Aronov <lordkuper@live.ru>
2026-06-03 13:51:15 +0300

    refactor: move assembly resolver initialization to separate file for clarity

--- Tests/AssemblyInfo.cs
+++ Tests/AssemblyInfo.cs
@@ -1,3 +1,13 @@
 using Xunit;
+using LordKuper.Common.Tests;
+
+// Force the AssemblyResolverInitialize static constructor to run before test discovery
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1806", Justification = "Forces static constructor")]
 
 [assembly: TestFramework("LordKuper.Common.Tests.RimWorldTestFramework", "LordKuper.Common.Tests")]
+
+// Trigger the assembly resolver initialization
+internal static class _InitializerTrigger
+{
+    static _InitializerTrigger() => _ = AssemblyResolverInitialize.Ready;
+}
